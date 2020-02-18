2a8d46e719ec7cea96b19316b3cb1c4eb1a8ce93
fishming <1661342068@qq.com>
2020-02-18 22:10:36 +0800

    renew the py

--- COOmake.py
+++ COOmake.py
@@ -2,16 +2,15 @@ from scipy import sparse
 import numpy as np
 import re
 
-np.set_printoptions(threshold='nan')
-
-n = 12
-m = 18
-Density=0.1
+n = 25
+m = 30
+Density=0.01
 matrixformat='coo'
 B = sparse.rand(m,n, density = Density, format = matrixformat, dtype = None)
 
 print(B)
 fo = open("sparse.txt", "w")
+fo.write("%d %d\n" % (m, n))
 for i in range(0, np.size(B.col)):
     fo.write("%s %s %s\n" % (B.col[i], B.row[i], B.data[i]))
 fo.close()
