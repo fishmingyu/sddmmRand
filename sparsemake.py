6210d2c2920d71ef289bb8b46abd8c0d3f4921ca
fishming <1661342068@qq.com>
2020-02-20 12:09:44 +0800

    add the way to read dense matrix

--- sparsemake.py
+++ sparsemake.py
@@ -3,25 +3,40 @@ import numpy as np
 
 # generate sddmm
 
-n = 25  # cols for sparse matrix S
-m = 40  # rows for sparse matrix S
-k = 35  # the cols for D1 & D2 transposed
+N = 25  # cols for sparse matrix S
+M = 40  # rows for sparse matrix S
+K = 35  # the cols for D1 & D2 transposed
 
 Density=0.01
 matrixformat='coo'
-B = sparse.rand(m,n, density = Density, format = matrixformat, dtype = None)
+B = sparse.rand(M,N, density = Density, format = matrixformat, dtype = None)
 
 # sddmm operations
-D1 = np.random.rand(m, k)
-D2 = np.random.rand(k, n)
+D1 = np.random.rand(M, K)
+D2 = np.random.rand(K, N)
 M1 = np.dot(D1, D2) # the dot product of dense matrix
 M2 = B.toarray()
 C = M1 * M2 # this is hadamard product of M1 and M2
 S = sparse.csr_matrix(C)    #to csr
 
+# note that the D1 dot with the D2 transposed 
+# so actually the data form should store the D2 transposed(NxK)
+fd1 = open("dense1.txt", "w")
+fd2 = open("dense2.txt", "w")
+for i in range(0, M):
+    for j in range(0, K):
+        fd1.write("%f " % (D1[i][j]))
+    fd1.write("\n")
+fd1.close()
+for i in range(0, K):
+    for j in range(0, N):
+        fd2.write("%f " % (D2[i][j]))
+    fd2.write("\n")
+fd2.close()
+
 # generate the COO form data
 fo = open("sparseCOO.txt", "w")
-fo.write("%d %d\n" % (m, n))
+fo.write("%d %d\n" % (M, N))
 for i in range(0, np.size(B.col)):
     fo.write("%s %s %s\n" % (B.col[i], B.row[i], B.data[i]))
 fo.close()
@@ -30,7 +45,7 @@ fo.close()
 B = B.tocsr()
 fo = open("sparseCSR.txt", "w")
 f1 = open("output.txt", "w")
-fo.write("%d %d\n" % (m, n))
+fo.write("%d %d %d\n" % (np.size(B.indptr), np.size(B.indices), np.size(B.data)))
 for i in range(0, np.size(B.indptr)):
     fo.write("%d " % (B.indptr[i]))
 fo.write("\n")
